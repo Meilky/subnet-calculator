86c90e874439eba5abb6baccd893ce4c1bb1e810
Meilky <mikaeljean2k3@gmail.com>
2022-03-31 18:59:17 -0400

    Added input

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ struct SubNet {
 }
 
 impl SubNet {
-    pub fn from(ip: Ip, cidr: Cidr) -> Result<Self,&'static str> {
+    pub fn from(ip: Ip, cidr: Cidr) -> Result<Self, &'static str> {
         let base: u32 = 2;
         let network: Ip;
         let first: Ip;
@@ -124,7 +124,7 @@ impl SubNet {
         })
     }
 
-    pub fn split_in_subs<'a>(&self, cidr: Cidr) -> Result<Vec<String>, &'a str> {
+    pub fn split_in_subs(&self, cidr: Cidr) -> Result<Vec<String>, &'static str> {
         let mut vec: Vec<String> = vec![];
 
         if cidr < self.cidr {
@@ -180,29 +180,95 @@ impl std::fmt::Display for SubNet {
     }
 }
 
-fn parse_ip(raw_ip: &String) -> Result<Ip, &str> {
-    let str_splice = raw_ip.split(".");
-    let nb: usize = 4;
+fn parse_ip(raw_ip: String) -> Result<Ip, String> {
+    let split_ip: Vec<&str> = raw_ip.split(".").collect();
 
-    if &str_splice.count() != &nb {
-        return Err("The inserted value isn't an proprely formated ip!");
+    if split_ip.len() != 4 {
+        return Err("The inserted value isn't an proprely formated ip!".to_string());
     }
 
-    Ok(45)
+    let mut ip: Ip = 0;
+    let base: u32 = 2;
+
+    for i in 0..4 {
+        let s = match split_ip.get(i) {
+            Some(v) => v,
+            _ => return Err(format!("Invalid ip at position (no value): {}", i)),
+        };
+
+        let value = match s.parse::<u32>() {
+            Ok(v) => v,
+            _ => return Err(format!("Invalid ip at position (no a int): {}", i)),
+        };
+
+        ip += value
+            * base.pow(
+                ((3 - i) * 8)
+                    .try_into()
+                    .expect("Error will transforming u8 to u32"),
+            );
+    }
+
+    Ok(ip)
+}
+
+fn parse_cidr(raw_cidr: String) -> Result<Cidr, String> {
+    let cidr = raw_cidr.parse::<u32>().expect("Cidr isn't a number");
+
+    if cidr > 32 {
+        return Err("The cidr should be under 32!".to_string());
+    }
+
+    Ok(cidr)
+}
+
+fn parse_min_nb(raw_nb: String, cidr: &Cidr) -> Result<u32, String> {
+    let base: u32 = 2;
+    let nb: u32 = raw_nb
+        .trim()
+        .parse::<u32>()
+        .expect("The min nb of ip is not a number");
+
+    if base.pow(32 - cidr) - 2 < nb {
+        return Err(
+            "The min nb cannot be bigger then the max number of usable ip of the network"
+                .to_string(),
+        );
+    }
+
+    Ok(nb)
+}
+
+fn parse_input(raw_ip: String, raw_nb: String) -> Result<(Ip, Cidr, u32), String> {
+    let split_ip: Vec<&str> = raw_ip.trim().split("/").collect();
+
+    if split_ip.len() != 2 {
+        return Err("The ip should be formated this way : <ip>/<cidr>".to_string());
+    }
+
+    let ip = parse_ip(split_ip.get(0).expect("No ip provided").to_string())?;
+    let cidr = parse_cidr(split_ip.get(1).expect("No cidr provided").to_string())?;
+    let min_nb: u32 = parse_min_nb(raw_nb, &cidr)?;
+
+    Ok((ip, cidr, min_nb))
 }
 
 fn main() {
     let mut raw_ip = String::new();
-
-    // Read ip from stdin
     println!("Ip address: ");
     stdin().read_line(&mut raw_ip).expect("failed to readline");
 
+    let mut raw_nb = String::new();
+    println!("Min nb address by subnet: ");
+    stdin().read_line(&mut raw_nb).expect("failed to readline");
+
+    let (ip, cidr, min_nb) = parse_input(raw_ip, raw_nb).unwrap();
+
+    println!("{},{},{}",ip,cidr,min_nb);
+
     let start = Instant::now();
 
-    let ip: Ip = parse_ip(&raw_ip).unwrap();
-
-    let subnet = match SubNet::from(ip, 8) {
+    let subnet = match SubNet::from(ip, cidr) {
         Ok(s) => s,
         Err(e) => panic!("{}", e),
     };
